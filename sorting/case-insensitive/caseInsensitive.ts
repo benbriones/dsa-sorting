1013a049ab2d37a6047606159f56f42c1e63d3ba
Ben Briones <benbriones50@gmail.com>
2024-04-26 10:47:24 -0700

    finished peopleSort and case sort

--- sorting/case-insensitive/caseInsensitive.ts
+++ sorting/case-insensitive/caseInsensitive.ts
@@ -1,7 +1,14 @@
 /** Sort array of strings in-place, case-insensitively. Returns array. */
 
 function caseInsensitiveSort(arr: string[]): string[] {
-  return [];
+  return arr.sort((a: string, b: string) => {
+    if (a.toLowerCase() < b.toLowerCase()) {
+      return -1;
+    } else if (a.toLowerCase() > b.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 export { caseInsensitiveSort };
--- sorting/people-sort/peopleSort.ts
+++ sorting/people-sort/peopleSort.ts
@@ -3,7 +3,19 @@ type tPerson = {fname: string, lname: string};
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return [];
+  return arr.sort((a: tPerson, b: tPerson) => {
+    const lNameA = a.lname.toLocaleLowerCase();
+    const lFirstA = a.fname.toLocaleLowerCase();
+    const lNameB = b.lname.toLocaleLowerCase();
+    const lFirstB = b.fname.toLocaleLowerCase();
+
+    if (lNameA !== lNameB) {
+      return lNameA < lNameB ? -1 : 1;
+    }
+
+    return lFirstA < lFirstB ? -1 : 1;
+
+  })
 }
 
 export { peopleSort };
