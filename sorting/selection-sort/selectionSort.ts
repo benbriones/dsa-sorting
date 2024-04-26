518d0b3942c684c880ac5f4c99d2986f07c1f86d
Jazz Cheema <jaskamalc@gmail.com>
2024-04-26 10:59:03 -0700

    finished exercise

--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,6 +1,17 @@
 /** Sort in-place and return sorted array. */
 
 function selectionSort(nums: number[]): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    let low = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[low] > nums[j]) {
+        low = j;
+      }
+    }
+    if (i !== low) {
+      [nums[i], nums[low]] = [nums[low], nums[i]];
+    }
+  }
   return nums;
 }
 
